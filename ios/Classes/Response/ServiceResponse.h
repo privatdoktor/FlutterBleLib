28d5030a230aacb303901bec3ee21d8c77bd8dfe
Oliver Kocsis <okocsis88@gmail.com>
2021-05-20 18:45:44 +0200

    removed old obj-c implementation

--- ios/Classes/Response/ServiceResponse.h
+++
@@ -1,4 +0,0 @@
-extern NSString * const SERVICE_RESPONSE_ID;
-extern NSString * const SERVICE_RESPONSE_UUID;
-extern NSString * const SERVICE_RESPONSE_DEVICE_ID;
-extern NSString * const SERVICE_RESPONSE_ID_PRIMARY;
